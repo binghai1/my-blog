98184cc8d1a271bd78ef44ee8a6c0635b502ffe4
“binghai1” <448456376@qq.com>
2019-08-03 23:40:28 +0800

    忽略vsconfig

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
 import {createStore,applyMiddleware,compose} from 'redux'
 import reducer from './reducer'
 import  thunk  from 'redux-thunk'
-import logger from 'redux-logger'
-const config=process.env.NODE_ENV==="develepment"
-  ?applyMiddleware(thunk,logger): applyMiddleware(thunk)
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(
-	config
+	applyMiddleware(thunk)
 ));
 
 export default store
