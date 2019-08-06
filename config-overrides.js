2585b3a838b5604e7b2831ee02c46f162f3aebcc
“binghai1” <448456376@qq.com>
2019-08-06 20:07:32 +0800

    再次修改

--- config-overrides.js
+++ config-overrides.js
@@ -1,46 +1,5 @@
 const { override, fixBabelImports, addLessLoader,addWebpackAlias,addDecoratorsLegacy } = require('customize-cra');
 const path = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
- 
-const addCustomize = () => config => {
-  if (process.env.NODE_ENV === 'production') {
-    config.devtool = false; //去掉map文件
-    if (config.plugins) {
-      config.plugins.push(new BundleAnalyzerPlugin());
-    }
-    // const splitChunksConfig = config.optimization.splitChunks;
-    //  if (config.entry && config.entry instanceof Array) {
-    //    config.entry = {
-    //      main: config.entry,
-    //      vendor: ["react", "react-dom", "react-router-dom", "react-redux", "redux", 'react-router-config',
-    //        "lodash", "moment", 'react-intl', 'react-addons-pure-render-mixin', 'redux-promise-middleware', "react-router", 
-    //      ]
-    //    }
-    //  } else if (config.entry && typeof config.entry === 'object') {
-    //    config.entry.vendor = ["react", "react-dom", "react-router-dom", "react-redux", "redux", 'react-router-config', 
-    //       "lodash", "moment", 'react-intl', 'react-addons-pure-render-mixin', 'redux-promise-middleware', "react-router", 
-    //    ];
-    //  }
- 
-    // Object.assign(splitChunksConfig, {
-    //   chunks: 'all',
-    //   cacheGroups: {
-    //     vendors: {
-    //       test: /node_modules/,
-    //       name: 'vendors',
-    //       priority: -10,
-    //     },
-    //     common: {
-    //       name: 'common',
-    //       minChunks: 2,
-    //       minSize: 30000,
-    //       chunks: 'all'
-    //     }
-    //   }
-    // })
-  }
-  return config;
-}
 
 module.exports = override(
   fixBabelImports('import', {
