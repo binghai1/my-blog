ca7842fbdc5765a8cb94b1043d5298a59b2f56fd
“binghai1” <448456376@qq.com>
2019-08-07 16:19:49 +0800

    修改冲突1133

--- src/router.js
+++ src/router.js
@@ -8,7 +8,6 @@ import  Edit from  './page/admin/edit'
 
 import  Manage from  './page/admin/manage'
 import  UserMange from  './page/admin/userManage'
-import  Article from  './page/article'
 import Admin from './page/admin'
 import Article from './page/article'
 const Main =asyncComponent(()=>import('./page/home/main'))
